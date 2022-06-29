5f6d28ef0ca29d6649f7d9109c6b2f85eafa97da
alexleon <alex.leon@aliyun.com>
2022-06-29 23:40:54 +0800

    rebuild standard maven project

--- src/main/java/leet_codes/main_bank/LeetCodes0034.java
+++ src/main/java/leet_codes/main_bank/LeetCodes0034.java
@@ -2,6 +2,7 @@ package leet_codes.main_bank;
 
 public class LeetCodes0034 {
     public int[] searchRange(int[] nums, int target) {
+
         return new int[]{-1,-1};
     }
 }
--- src/test/java/leet_codes/main_bank/LeetCodes0034Test.java
+++ src/test/java/leet_codes/main_bank/LeetCodes0034Test.java
@@ -1,18 +1,15 @@
 package leet_codes.main_bank;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
 
 class LeetCodes0034Test {
-
-    @org.junit.jupiter.api.BeforeEach
+    @Before
     void setUp() {
     }
-
-    @org.junit.jupiter.api.AfterEach
-    void tearDown() {
-    }
-
-    @org.junit.jupiter.api.Test
+    @Test
     void searchRange() {
+        Assert.assertEquals(1,1);
     }
 }
