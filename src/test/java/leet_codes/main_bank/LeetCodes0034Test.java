e62c85fde3b9a7460450286df7faad1346440728
alexleon <alex.leon@aliyun.com>
2022-06-29 23:06:12 +0800

    change to maven project

---
+++ src/test/java/leet_codes/main_bank/LeetCodes0034Test.java
@@ -0,0 +1,18 @@
+package leet_codes.main_bank;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeetCodes0034Test {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void searchRange() {
+    }
+}
